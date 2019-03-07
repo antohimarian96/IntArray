5848d4fbeec7a32b61ac20285860009ec3f4efa1
antohimarian96 <marian_antohi96@yahoo.com>
2019-03-07 11:05:04 +0200

    Program

---
+++ ArrayFacts.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using Array;
+
+namespace ArrayFacts
+{
+    public class ArrayFacts
+    {
+        [Fact]
+        public void ArrayTests()
+        {
+            var array = new IntArray();
+            array.Add(1);
+            array.Add(2);
+            int count = array.Count();
+            Assert.Equal(2, count);
+            int elementTwo = array.Element(1);
+            Assert.Equal(2, elementTwo);
+            array.SetElement(1, 3);
+            Assert.True(array.Contains(3));
+            Assert.Equal(1, array.IndexOf(3));
+            array.Insert(1, 2);
+            Assert.Equal(3, array.Count());
+            array.Add(4);
+            array.Add(7);
+            array.Add(4);
+            array.Add(5);
+            array.Remove(4);
+            array.RemoveAt(6);
+            Assert.Equal(6, array.Count());
+        }
+    }
+}
---
+++ IntArray.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    public class IntArray
+    {
+        private int[] array;
+
+        public IntArray()
+        {
+            array = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            System.Array.Resize(ref array, array.Length+1);
+            array[array.Length - 1] = element;
+        }
+
+        public int Count()
+        {
+            return array.Length;
+        }
+
+        public int Element(int index)
+        {
+            return array[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            array[index] = element;
+        }
+
+        public bool Contains(int element)
+        {
+            bool containsValidation = false;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    containsValidation = true;
+            return containsValidation;
+        }
+
+        public int IndexOf(int element)
+        {
+            return System.Array.IndexOf(array, element);
+        }
+
+        public void Insert(int index, int element)
+        {
+            int[] auxiliarArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                auxiliarArray[i] = array[i];
+            System.Array.Resize(ref array, array.Length + 1);
+            for (int i = index; i < array.Length - 1; i++)
+                array[i + 1] = auxiliarArray[i];
+            array[index] = element;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = 0;
+        }
+
+        public void Remove(int element)
+        {
+            int[] auxiliarArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                auxiliarArray[i] = array[i];
+            bool found = false;
+            int index = 0;
+            for (int i = 0; i < array.Length && !found; i++)
+                if (array[i] == element)
+                {
+                    index = i;
+                    found = true;
+                    array[i] = 0;
+                }
+            for (int i = index; i < array.Length-1; i++)
+                array[i] = auxiliarArray[i + 1];
+            System.Array.Resize(ref array, array.Length - 1);
+        }
+
+        public void RemoveAt(int index)
+        {
+            int[] auxiliarArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                auxiliarArray[i] = array[i];
+            for (int i = index; i < array.Length - 1; i++)
+                array[i + 1] = auxiliarArray[i];
+            System.Array.Resize(ref array, array.Length - 1);
+        }
+    }
+}
