cf92ea183ba1c4a61e4256a8ae2dcaba8e0921d1
antohimarian96 <46729207+antohimarian96@users.noreply.github.com>
2019-03-13 11:14:56 +0200

    Add files via upload

--- IntArray.cs
+++ IntArray.cs
@@ -3,22 +3,20 @@
     public class IntArray
     {
         private int[] array;
-        private int count;
+        public int Count { get; private set; }
 
         public IntArray()
         {
             array = new int[4];
-            count = 0;
+            Count = 0;
         }
 
         public void Add(int element)
         {
             EnsureCapacity();
-            array[count] = element;
-            count++;
+            array[Count] = element;
+            Count++;
         }
-
-        public int Count { get => count; }
 
         public int this[int index]
         {
@@ -33,7 +31,7 @@
 
         public int IndexOf(int element)
         {
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < Count; i++)
                 if (array[i] == element)
                     return i;
             return -1;
@@ -45,21 +43,21 @@
             for (int i = array.Length-1 ; i >= index; i--)
                 array[i] = array[i-1];
             array[index] = element;
-            count++;
+            Count++;
         }
 
         public void Clear()
         {
-            count = 0;
+            Count = 0;
         }
 
         public void RemoveAt(int index)
         {
-            if (index <= count)
+            if (index <= Count)
             {
                 for (int i = index; i < array.Length - 1; i++)
                     array[i] = array[i + 1];
-                count--;
+                Count--;
             }
         }
 
@@ -70,7 +68,7 @@
 
         public void EnsureCapacity()
         {
-            if (count == array.Length)
+            if (Count == array.Length)
                 System.Array.Resize(ref array, array.Length * 2);
         }
 
